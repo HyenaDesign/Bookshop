0ddae7ca35dbbe0656c4e5f5d014e9797af3b057
Sam Apers <sam.hyenadesign@gmail.com>
2023-11-27 12:41:05 +0100

    added a book section

--- App.js
+++ App.js
@@ -4,7 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Bookshop</Text>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Book Title 1</Text>
+        <Text style={styles.sectionDesc}>by Some Random Dude</Text>
+        <Text style={styles.sectionInfo}>Lorem ipsum dolores text random shit omdat ik geen lorem ipsum generator wil opzoeken, ben momenteel te lui. Dus daarom schrijf ik even deze random bs.</Text>
+        <Text style={styles.bookCode}>ISBN 978-8-2165-1860-1</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +20,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  title: {
+    alignItems: 'flex-start', 
+    justifyContent: 'flex-start',  
+    paddingLeft: 10,  
+    marginTop: 50,
+    fontSize: 36,
+    fontWeight: 'bold',
+    marginBottom: 40
+  },
+  section: {
+    padding: 5,
+    margin: 5
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  sectionDesc: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 15
   },
 });
